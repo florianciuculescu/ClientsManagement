2ae92a73fd84a2547d80682d2d5b984ca73ade25
fciuculescu <fciuculescu@deloittece.com>
2020-10-02 09:04:09 +0300

    project setup

---
+++ src/main/java/com/example/clientsmanagement/ClientsmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clientsmanagement/ClientsmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
